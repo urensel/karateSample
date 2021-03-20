a159681a34159f2f4b0440bf6aa54638f9637ce3
gururorkunurensel <gururorkun.urensel@gmail.com>
2021-03-20 03:29:21 +0300

    added pom

--- src/test/java/features/TestRunner.java
+++ src/test/java/features/TestRunner.java
@@ -16,43 +16,44 @@ import org.junit.jupiter.api.Test;
 
 public class TestRunner {
 
-//    @Karate.Test
-//    Karate testReqres() {
-//        return Karate.run("reqres").relativeTo(getClass());
-//    }
-//
-//    @Karate.Test
-//    Karate testGitHub() {
-//        return Karate.run("github").relativeTo(getClass());
-//    }
-//
+/*    @Karate.Test
+    Karate testReqres() {
+        return Karate.run("reqres").relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate testGitHub() {
+        return Karate.run("github").relativeTo(getClass());
+    }
+
     @Karate.Test
     Karate testAll() {
         return Karate.run("classpath:features").relativeTo(getClass());
+    }*/
 
+
+    @Test
+    void testParallel() {
+        Results results = Runner.path("classpath:features").parallel(5);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
 
+/*
+    @Test
+    void testParallel() {
+        Results results = Runner.path("classpath:features").parallel(5);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+        generateReport(results.getReportDir());
+    }
 
-//    @Test
-//    void testParallel() {
-//        Results results = Runner.path("classpath:features").parallel(5);
-//        assertEquals(0, results.getFailCount(), results.getErrorMessages());
-//    }
-
-//    @Test
-//    void testParallel() {
-//        Results results = Runner.path("classpath:features").parallel(5);
-//        assertEquals(0, results.getFailCount(), results.getErrorMessages());
-//        generateReport(results.getReportDir());
-//    }
-
-//    public static void generateReport(String karateOutputPath) {
-//        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
-//        List<String> jsonPaths = new ArrayList(jsonFiles.size());
-//        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
-//        Configuration config = new Configuration(new File("target"), "demo");
-//        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
-//        reportBuilder.generateReports();
-//    }
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "demo");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+    }
+*/
 
 }
